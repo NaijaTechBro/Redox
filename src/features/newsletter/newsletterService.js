cc5184f94731ca933a9366a1375bffe254d85b57
NaijaTechBro <bakisodiq@gmail.com>
2023-06-14 01:51:15 +0100

    service

---
+++ src/features/newsletter/newsletterService.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
+export const API_URL = `${BACKEND_URL}/api/v1/newsletter`;
+// Newsletter
+const newsletter = async (userData) => {
+    const response = await axios.post(API_URL, userData)
+    
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+
+    return response.data
+}
+
+const newsletterService = {
+        newsletter,
+}
+
+export default newsletterService;
