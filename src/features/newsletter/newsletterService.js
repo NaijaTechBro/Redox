084f54b08875073a88777404633a98d5fc8b5617
NaijaTechBro <bakisodiq@gmail.com>
2023-06-14 02:47:33 +0100

    jjj

--- src/features/newsletter/newsletterService.js
+++ src/features/newsletter/newsletterService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const BACKEND_URL = import.meta.env.VITE_APP_BACKEND_URL;
-export const API_URL = `${BACKEND_URL}/api/v1/newsletter`;
+export const API_URL = `${BACKEND_URL}/api/v1/newsletter/join`;
 // Newsletter
 const newsletter = async (userData) => {
     const response = await axios.post(API_URL, userData)
