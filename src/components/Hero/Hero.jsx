0a97e6ec1cea2f5422308d7fcda0bd3cf31a0eb9
NaijaTechBro <bakisodiq@gmail.com>
2023-06-14 01:50:45 +0100

    hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { toast } from 'react-toastify'
-import { Subscribe, reset } from '../../features/subscribe/subscribeSlice'
+import { Newsletter, reset } from '../../features/newsletter/newsletterSlice'
 import './hero.css'
+import Loading from '../Loader/Loader'
+import Modal from '../Modal/Modal'
+
+import { useState } from 'react'
 
 const Hero = () => {
+  const [getModal, setGetModal] = useState(false)
+
+  const [newsletter, setNewsletter] = useState('')
+  const [isOpen, setisOpen] = useState(false)
+  const dispatch = useDispatch()
+
+  const [email, setEmail] = useState('')
+  const { isLoading } = useSelector((state) => state.newsletter)
+
+  const header = async (e) => {
+    e.preventDefault()
+    if(!email) {
+      return toast.error('Please enter your email')
+    }
+
+    const userData = {
+      email,
+    }
+
+    await dispatch(Newsletter(userData))
+    await dispatch(reset())
+  }
+
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
     <div className="section">
       <div className="left">
@@ -12,15 +43,16 @@ const Hero = () => {
           <p>
             Get Weekly News, Insights and Research about Crypto, Economy and Financial knowledge.
           </p>
-                <form>
+                <form onSubmit={header}>
                   <input
                     type='email'
                     id='email'
                     name='email'
                     placeholder='    Enter Email Address'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                   />
-                  <button onClick={() => setGetModal(!getM
-                    )}>Join <span className='desktop'>Free</span>{' '}
+                  <button onClick={() => setGetModal(!getModal)}>Join <span className='desktop'>Free</span>{' '}
                     </button>
                 </form>
                 <p>Join over 2k+ readers. 100% free, unsubscribe anytime.</p>
@@ -28,6 +60,11 @@ const Hero = () => {
       <section className="right">
       <img src="https://res.cloudinary.com/dkcazf954/image/upload/v1682880617/redox-mockup_foog8w.png" />
       </section>
+      {getModal && (
+        <div>
+          <Modal closeModal={() => setGetModal(false)} getModal={getModal} />
+          </div>
+      )}
     </div>
   )}
 
