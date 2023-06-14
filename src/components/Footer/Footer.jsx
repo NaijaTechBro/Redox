f13dba7cc638aab4fbfa2249d6eec7dca0c82302
NaijaTechBro <bakisodiq@gmail.com>
2023-06-14 11:59:17 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './footer.css'
-import { FaTwitter, FaLinkedin } from "react-icons/fa"
+import { FaTwitter, FaLinkedin, FaTelegramPlane } from "react-icons/fa"
 import { AiOutlineMail } from "react-icons/ai"
 
 const Footer = () => {
@@ -15,7 +15,7 @@ const Footer = () => {
       <li><a href='/about'>About</a></li>
       <li><a href='/crypto-news'>Crypto News</a></li>
       <li><a href='/free-resources'>Free Resources</a></li>
-      <li><a href='http://t.me/insideredox' target='blank'>Community</a></li>
+      <li><a href='https://t.me/insideredox' target='blank'>Community</a></li>
     </ul>
     <div className="copyright">
   <p>© 2023 RedoX Trading, Inc. | 
@@ -28,10 +28,11 @@ const Footer = () => {
     <ul className="social-media">
       <li><a href="https://twitter.com/insideredox" target='blank'><FaTwitter /></a></li>
       {/* <li><a href="#"><FaLinkedin /></a></li> */}
-      <li><a href="mailto:me@redox.com.ng" target='blank'><AiOutlineMail /></a></li>
+      <li><a href='https://t.me/insideredox' target='blank'><FaTelegramPlane /></a></li>
+      <li><a href="mailto:insideredox@gmail.com" target='blank'><AiOutlineMail /></a></li>
     </ul>
     <div className="button">
-    <button>Subscribe</button>
+    <button><a href="/"></a>Subscribe</button>
     </div>
   </div>
   
