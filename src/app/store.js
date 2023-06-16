1bf4dfeb1f7ee2ae64d4ed2386846411e09e9e05
NaijaTechBro <bakisodiq@gmail.com>
2023-06-17 00:32:49 +0100

    s

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import newsletterReducer from '../features/newsletter/newsletterSlice'
-
-export const store = configureStore({
-    reducer: {
-        newsletter: newsletterReducer,
-    },
-})
