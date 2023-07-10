7996a9399f2472b33184d5b1a5278240159b5dd1
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-07-11 00:35:36 +0100

    postslice

--- src/redux/features/post/postsSlice.js
+++ src/redux/features/post/postsSlice.js
@@ -7,18 +7,18 @@ const initialState = {
     post: null,
     posts: [],
     isError: false,
-    isLoading: false,
     isSuccess: false,
+    isLoading: false,
     message: "",
-    category: []
+    category: [],
   };
 
 
 
 // Create Post
 export const createPost = createAsyncThunk(
-    'posts/createPost',
-     async (formData, thunkAPI) => {
+    'posts/create',
+    async (formData, thunkAPI) => {
         try {
             return await postService.createPost(formData);
         } catch (error) {
@@ -26,54 +26,59 @@ export const createPost = createAsyncThunk(
             (error.response &&
                 error.response.data &&
                 error.response.data.message) ||
-             error.message ||
-             error.toString();
+            error.message ||
+            error.toString();
             return thunkAPI.rejectWithValue(message);
-           }
+          }
         }
     );
 
 // ACTIONS
-const postsSlice = createSlice({
+const postSlice = createSlice({
   name: 'post',
   initialState,
   reducers: {
-    reset: (state) => {
-      state.isError = false;
-      state.isSuccess = false;
-      state.isLoading = false;
-      state.message = "";
-      state.twoFactor = false
-    },
+    CALC_CATEGORY(state, action) {
+      const posts = action.payload;
+      const array = [];
+      posts.map((item) => {
+        const { category } = item;
 
+        return array.push(category);
+      });
+      const uniqueCategory = [...new Set(array)];
+      state.category = uniqueCategory;
+    },
   },
+
   extraReducers: (builder) => {
     builder
     // Create Post
       .addCase(createPost.pending, (state) => {
-        state.isLoading = true
+        state.isLoading = true;
       })
       .addCase(createPost.fulfilled, (state, action) => {
         state.isLoading = false,
         state.isSuccess = true,
-        state.isLoggedIn = true,
-        state.post = action.payload,
+        state.isError = false,
+        console.log(action .payload),
+        state.posts.push(action.payload),
         toast.success("Post Created Successfully")
       })
       .addCase(createPost.rejected, (state, action) => {
         state.isLoading = false,
         state.isError = true,
         state.message = action.payload,
-        state.post = null,
-        toast.error(state.message)
+        toast.error(action.payload);
       })
   },
 });
 
-export const { reset } =
-  postsSlice.actions;
+export const { CALC_CATEGORY } =
+  postSlice.actions;
 
-export const selectIsLoggedIn = (state) => state.posts.isLoggedIn;
-export const selectUser = (state) => state.posts.user;
+  export const selectIsLoading = (state) => state.post.isLoading;
+  export const selectPost = (state) => state.post.post;
+  export const selectCategory = (state) => state.post.category;
 
-export default postsSlice.reducer;
+export default postSlice.reducer;
