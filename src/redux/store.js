3d36c8d461aaf4d7eaa2035f0787c028d0745e54
NaijaTechBro <bakisodiq@gmail.com>
2023-07-10 18:02:10 +0100

    store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import newsletterReducer from '../redux/features/newsletter/newsletterSlice';
 import authReducer from '../redux/features/auth/authSlice';
+import postsReducer from '../redux/features/post/postsSlice';
 
 
 export const store = configureStore({
     reducer: {
         newsletter: newsletterReducer,
         auth: authReducer,
+        posts: postsReducer,
     },
 })
