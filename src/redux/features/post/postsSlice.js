2bc67da530d7bfd1d80d9bca36962609b99c0b43
NaijaTechBro <bakisodiq@gmail.com>
2023-07-10 18:02:44 +0100

    postsSlice

---
+++ src/redux/features/post/postsSlice.js
@@ -0,0 +1,79 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from "react-toastify";
+import postService from '../post/postService';
+
+
+const initialState = {
+    isLoggedIn: true,
+    post: null,
+    posts: [],
+    isError: false,
+    isLoading: false,
+    isSuccess: false,
+    message: "",
+  };
+
+
+
+// Create Post
+export const createPost = createAsyncThunk(
+    'posts/createPost',
+     async (post, thunkAPI) => {
+        try {
+            return await postService.createPost(post);
+        } catch (error) {
+            const message = 
+            (error.response &&
+                error.response.data &&
+                error.response.data.message) ||
+             error.message ||
+             error.toString();
+            return thunkAPI.rejectWithValue(message);
+           }
+        }
+    );
+
+// ACTIONS
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = "";
+      state.twoFactor = false
+    },
+
+  },
+  extraReducers: (builder) => {
+    builder
+    // Create Post
+      .addCase(createPost.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(createPost.fulfilled, (state, action) => {
+        state.isLoading = false,
+        state.isSuccess = true,
+        state.isLoggedIn = true,
+        state.post = action.payload,
+        toast.success("Post Created Successfully")
+      })
+      .addCase(createPost.rejected, (state, action) => {
+        state.isLoading = false,
+        state.isError = true,
+        state.message = action.payload,
+        state.post = null,
+        toast.error(state.message)
+      })
+  },
+});
+
+export const { reset } =
+  postsSlice.actions;
+
+export const selectIsLoggedIn = (state) => state.posts.isLoggedIn;
+export const selectUser = (state) => state.posts.user;
+
+export default postsSlice.reducer;
