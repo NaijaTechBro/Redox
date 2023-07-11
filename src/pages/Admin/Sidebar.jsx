17e70bbd3cceab9a3d5bfab88c2ce63140ca7795
Sodiq Baki Adeiza <58841467+NaijaTechBro@users.noreply.github.com>
2023-07-11 01:30:55 +0100

    sidebar

--- src/pages/Admin/Sidebar.jsx
+++ src/pages/Admin/Sidebar.jsx
@@ -5,6 +5,7 @@ import { logout, RESET } from '../../redux/features/auth/authSlice';
 import './admin.css'
 
 const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(false);
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
@@ -12,15 +13,24 @@ const Sidebar = () => {
         navigate("/admin/home")
     };
 
+    const handleToggle = () => {
+        setIsOpen(!isOpen);
+    };
+    
     const logoutUser = () => {
         dispatch(RESET());
         dispatch(logout());
         navigate("/admin/login");
-      };
+    };
 
 
-  return (
-    <div className="sidebar">
+return (
+    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+        <div className="hamburger" onClick={handleToggle}>
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
     <div className="sidebar-top"onClick={goHome}>
         <i className="logo fab fa-sketch"></i>
         <Link to='/admin/home'><img src='https://res.cloudinary.com/dkcazf954/image/upload/v1682628918/redoxlogo_1_cfljyp.png' alt='redox-logo' /></Link>
@@ -57,7 +67,7 @@ const Sidebar = () => {
             </li>
             <li className="list-item">
                 <i className="list-item-icon fas fa-users"></i>
-                <button className='sidebar-button' onClick={logoutUser}><span className="list-item-text">Logout</span></button>
+                <Link onClick={logoutUser}><span className="list-item-text">Logout</span></Link>
             </li>
         </ul>
     </div>
